63e5044a885c3a04002fb209120da7df671442e3
XDaggethX <dagetprepa@hotmail.com>
2023-11-20 20:06:18 -0600

    interfaces

--- proyecto/src/app/pages/auth/forgot-password/forgot-password.page.ts
+++ proyecto/src/app/pages/auth/forgot-password/forgot-password.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.page.scss'],
 })
 export class ForgotPasswordPage implements OnInit {
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email])
+  })
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  submit(){
+    if(this.form.valid){
+      console.log(this.form.value)
+    }
   }
 
 }
--- proyecto/src/app/pages/auth/sing-up/sing-up.page.ts
+++ proyecto/src/app/pages/auth/sing-up/sing-up.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sing-up',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sing-up.page.scss'],
 })
 export class SingUpPage implements OnInit {
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+
+  })
 
   constructor() { }
 
   ngOnInit() {
+  }
+    submit(){
+    if(this.form.valid){
+      console.log(this.form.value)
+    }
   }
 
 }
